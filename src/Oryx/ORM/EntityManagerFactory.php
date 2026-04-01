b9fe8297a1206badc8a1c226e18a034decf4f0ce
Povilas Brilius <pbrilius@gmail.com>
2026-04-01 21:23:13 +0300

    Iž sankabos disqussas lauke.

--- src/Oryx/ORM/EntityManagerFactory.php
+++ src/Oryx/ORM/EntityManagerFactory.php
@@ -22,7 +22,7 @@ class EntityManagerFactory
     {
         $envConfig = new EnvironmentConfig();
 
-        return self::create($envConfig->getDatabaseParams(), dirname(__DIR__, 2) . '/schema');
+        return self::create($envConfig->getDatabaseParams(), dirname(__DIR__, 3) . '/schema');
     }
 
     /**
